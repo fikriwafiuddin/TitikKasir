031f1f8a2fefe05f33b831f3cfa51bf84b908ca0
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2026-02-13 21:12:29 +0700

    fix(backend): fix entrypoint app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,10 @@ import logger from "./utils/logger.js"
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  logger.info(`[Server] Server is running at http://localhost:${port}`)
-})
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    logger.info(`[Server] Server is running at http://localhost:${port}`)
+  })
+}
+
+export default app
